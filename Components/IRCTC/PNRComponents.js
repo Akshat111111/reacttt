c7ca66e588ebad0e5b8568ee72b0c410d79a74f0
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-05-30 18:42:44 +0530

    Update PNRComponents.js

--- Components/IRCTC/PNRComponents.js
+++ Components/IRCTC/PNRComponents.js
@@ -1,3 +1,4 @@
+/*
 import axios from "axios";
 import React from "react";
 import './PNRStyle.css'
@@ -129,4 +130,122 @@ class PNRComponent extends React.Component {
     }
 }
 
+export default PNRComponent; 
+
+*/ 
+
+
+import React, { useState } from "react";
+import axios from "axios";
+import './PNRStyle.css';
+
+const PNRComponent = () => {
+    const [PNRNumber, setPNRNumber] = useState("");
+    const [PNRDetails, setPNRDetails] = useState([]);
+    const [PassengerStatus, setPassengerStatus] = useState([]);
+    const [OnButtonClicked, setOnButtonClicked] = useState(false);
+    const [ErrorMessage, setErrorMessage] = useState("");
+    const [IsErrorOccurred, setIsErrorOccurred] = useState(false);
+
+    const handleChange = (event) => {
+        setPNRNumber(event.target.value);
+    }
+
+    const handleSubmit = () => {
+        setPNRDetails([]);
+        setPassengerStatus([]);
+        setErrorMessage("");
+        setIsErrorOccurred(false);
+
+        const options = {
+            method: 'GET',
+            url: 'api/Employee/GetPnrDetails',
+            params: { pnrNumber: PNRNumber },
+            headers: {
+                'X-RapidAPI-Key': '08d558a2f2msh89912308fa1d13fp16e322jsn23960f0abdbe',
+                'X-RapidAPI-Host': 'irctc1.p.rapidapi.com'
+            }
+        }
+
+        axios.request(options)
+            .then(response => {
+                console.log(response.data);
+                setPNRDetails(response.data.data);
+                setPassengerStatus(response.data.data.PassengerStatus);
+                setOnButtonClicked(true);
+            })
+            .catch(error => {
+                setErrorMessage("Please enter the correct PNR...");
+                setIsErrorOccurred(true);
+            });
+    }
+
+    return (
+        <div className="app">
+            <h2>This is Engineer's desk Railway Website</h2>
+            <h3>Please enter your PNR Number</h3>
+            <div>
+                <input type="text" id="pnr" name="pnr" value={PNRNumber} onChange={handleChange} />
+                <button type="submit" onClick={handleSubmit}>Search</button>
+            </div>
+
+            {IsErrorOccurred ? <h5 className="errorMessage">{ErrorMessage}</h5> : OnButtonClicked &&
+                <div>
+                    <div>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>PNR Number</th>
+                                    <th>Train Number</th>
+                                    <th>Train Name</th>
+                                    <th>Source Station</th>
+                                    <th>Destination Station</th>
+                                    <th>Date of Journey</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{PNRDetails.Pnr}</td>
+                                    <td>{PNRDetails.TrainNo}</td>
+                                    <td>{PNRDetails.TrainName}</td>
+                                    <td>{PNRDetails.BoardingStationName}</td>
+                                    <td>{PNRDetails.ReservationUptoName}</td>
+                                    <td>{PNRDetails.SourceDoj}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Person No.</th>
+                                    <th>Coach</th>
+                                    <th>Berth</th>
+                                    <th>Booking Status</th>
+                                    <th>Current Status</th>
+                                    <th>Percentage Prediction</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {PassengerStatus.map((passenger, index) =>
+                                    <tr key={index}>
+                                        <td>{passenger.Number}</td>
+                                        <td>{passenger.Coach}</td>
+                                        <td>{passenger.Berth}</td>
+                                        <td>{passenger.BookingStatus}</td>
+                                        <td>{passenger.CurrentStatus}</td>
+                                        <td>{passenger.PredictionPercentage}</td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            }
+        </div>
+    );
+}
+
 export default PNRComponent;
+
