eb81c2424ba2a014070e19aeef73e06b25cd66bd
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-06-14 17:43:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import PNRComponent from "./Components/IRCTC/PNRComponent";
+
+import PNRComponent from "./Components/IRCTC/PNRComponents";
+
+const App=()=>
+{
+  return(
+    <div>
+     <PNRComponent/>
+    </div>
+  )
+}
+
+export default App;
